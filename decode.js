f45b1d23219daed83b240370aaf5952ed2231cbe
3Ti <3Ti.Site@gmail.com>
2023-10-25 18:50:01 +0800

    ♨

--- decode.js
+++ decode.js
@@ -1,9 +1,9 @@
-import Reader from './reader.js'
+import Reader from './decode/reader.js'
 
 export default (func_li, repeated_id_li)=>(msg)=>{
-  var li = [],n,data;
+  var li = [],n,data,o;
   for ([n,{data}] of Reader(msg)) {
-    o = func_li[--n]({data})
+    o = func_li[--n](data)
     if(repeated_id_li.includes(n)) {
       if(li[n]){
         li[n].push(o);
