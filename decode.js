9cc671dbdb403e89d18bca659b252037c7be7868
3Ti <3Ti.Site@gmail.com>
2023-10-26 11:47:02 +0800

    ♨

--- decode.js
+++ decode.js
@@ -1,18 +1,42 @@
 import Reader from './decode/reader.js'
 
-export default (func_li, default_li)=>(msg)=>{
-  var n,data,o,t,li=structuredClone(default_li);
-
-  for ([n,{data}] of Reader(msg)) {
-    o = func_li[--n](data);
-    t = li[n];
-
-    if(Array.isArray(t)) {
-      t.push(o)
-    } else {
-      li[n] = o
+export default (func_li, default_li)=>{
+  var array_pos=new Set();
+  default_li.forEach(
+    (v,p)=>{
+      if(Array.isArray(v)){
+        array_pos.add(p);
+      }
     }
-  }
+  );
+  return (msg)=>{
+    var o,n,data,t,li=[];
 
-  return li
+    for ([n,{data}] of Reader(msg)) {
+      o = func_li[--n](data);
+      t = li[n];
+
+      if(array_pos.has(n)) {
+        data = li[n];
+        if(data){
+          data.push(o)
+        }else{
+          li[n] = [o]
+        }
+      } else {
+        li[n] = o
+      }
+    }
+    
+    default_li.forEach((v,p)=>{
+      if(li[p]===undefined){
+        if(array_pos.has(p)){
+          v = [];
+        }
+        li[p] = v;
+      }
+    });
+
+    return li
+  }
 }
